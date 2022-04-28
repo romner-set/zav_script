0d720249756bfa14c2b7992121ae82d8727c6472
Romner <roman@skotnica.com>
2022-04-28 19:55:55 +0200

    dfdf

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use headless_chrome::{Browser, LaunchOptionsBuilder};
 use sysinputs::keyboard::{Key, Physical};
 
 const LOGIN: &str = "https://student.zav.cz/#!/login";
-const RESULTS: &str = "#views-placeholder > div:nth-child(3) > div.d-flex.h-100.flex-column.bg-white.border-top.border-primary.border-width-4 > div.zav-course-content > view4 > div.zav-fixed-height > lesson-panel > div > div > div.col-6.pr-5.d-flex.flex-wrap.align-content-start.h-100";
+const NEXT: &str = "div.zav-fixed-height > lesson-panel > div > div > div.col-6.d-flex.justify-content-end.align-items-center.p-0 > button:nth-child(2)";
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     println!("Opening chrome...");
@@ -15,7 +15,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
             None => None,
         }).build()?
     )?;
-    let tab = *browser.wait_for_initial_tab()?;
+    let tab = browser.wait_for_initial_tab()?;
     tab.set_default_timeout(Duration::MAX);
 
     println!("Opened, navigating to {}", LOGIN);
@@ -29,43 +29,53 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
             "https://student.zav.cz/#!/courses" => {
                 println!("Success, waiting for course selection.");
                 let input = tab.wait_for_element("div > text-input > div#textInput")?;
-                let results = tab.find_element(RESULTS)?;
                 println!("Selected. Typing, don't move your mouse...");
+                //thread::park();
 
                 input.click()?;
-                'outer: loop {
-                    let mut i = 0;
-                    let mut last = 0;
-                    loop {
-                        let elem = tab.find_element("div#scrollableText > div > p")?.get_description()?.children.unwrap();
-                        let mut spaces = elem[0].node_value.match_indices(' ');
-        
-                        let text = if let Some((j, _)) = spaces.nth(i) {
-                            let ret = &elem[0].node_value[last..j];
-                            last = j;
-                            ret
-                        } else {
-                            let text = &elem[0].node_value[last..elem[0].node_value.len()-2]; //'¶' has 2 bytes
-                            println!("{}", text);
-            
-                            sysinputs::keyboard::send_str(text);
-                            sysinputs::keyboard::send_key(Key::Physical(Physical::Return));
-                            thread::sleep(Duration::from_millis(100));
-                            break
-                        };
-        
+                
+                let mut i = 0;
+                let mut last = 0;
+                loop {
+                    let elem = tab.find_element("div#scrollableText > div > p")?.get_description()?.children.unwrap();
+                    let mut spaces = elem[0].node_value.match_indices(' ');
+    
+                    if let Some((j, _)) = spaces.nth(i) {
+                        let text = &elem[0].node_value[last..j];
+                        last = j;
+    
                         print!("{}", text); io::stdout().flush()?;
         
                         sysinputs::keyboard::send_str(text);
                         i+=1;
+                    } else {
+                        //println!("\nEND");
 
-                        if !results.get_description()?.children.unwrap().is_empty() {break 'outer}
-                        //else {println!("{:#?}", tab.find_element(RESULTS)?.get_description()?.children.unwrap());}
+                        let text = &elem[0].node_value[last..];
 
-                        thread::sleep(Duration::from_millis(50));
-                    }
+                        for ch in text.chars() {
+                            sysinputs::keyboard::send_key(if ch == '¶' {
+                                Key::Physical(Physical::Return)
+                            } else {
+                                Key::Unicode(ch)
+                            });
+                        }
+
+                        println!("{}", text);
+
+                        thread::sleep(Duration::from_secs(1));
+                        if tab.find_element("div#scrollableText > div > p")?.get_description()?.children.unwrap()[0].node_value.match_indices(' ')
+                        .nth(i).is_none() {break}
+                        //tab.find_element(NEXT)?.click()?;
+                        //thread::park();
+                        //break
+                    };
+
+                    //if let Ok(b) = tab.find_element(NEXT) {println!("Selecting next course."); b.click()?; break;}
+                    //else {println!("{:#?}", tab.find_element(RESULTS)?.get_description()?.children.unwrap());}
+
+                    //thread::sleep(Duration::from_millis(50));
                 }
-                break
             },
             _ => {
                 println!("Please login.");
@@ -73,8 +83,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
             }
         }
     }
-    
-    thread::park();
-
-    Ok(())
 }
